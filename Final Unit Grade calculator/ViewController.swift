7ef1217daa127ee8f9f9b72bb4a94e9cae0290fb
CAMERON TOPPEL <rl107019@rlas-macair-34.rlas-116.org>
2018-11-12 10:31:26 -0600

    Initial Commit

---
+++ Final Unit Grade calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Unit Grade calculator
+//
+//  Created by CAMERON TOPPEL on 11/12/18.
+//  Copyright © 2018 CAMERON TOPPEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
